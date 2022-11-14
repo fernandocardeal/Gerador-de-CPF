215498126e1c421a7f212aaf55d367b40ccb09de
Luis Fernando Santos Cardeal <fernandocardeal529@gmail.com>
2022-11-14 01:53:21 -0300

    Commit 2 (14/11/2022): Aprimoramento do Projeto

---
+++ App.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from cpf import CPF
+from pyUFbr.baseuf import ufbr
+
+cpf_tool = CPF()
+
+def App():
+    st.title('CPF Tools')
+    with st.sidebar:
+        opcao = st.radio('Escolha a ferramenta', ['Gerar CPF', 'Validar CPF'])
+    if opcao == 'Validar CPF':
+        Validar_CPF()
+    elif opcao == 'Gerar CPF':
+        Gerar_CPF()
+
+def Validar_CPF():
+    st.subheader('Validador de CPF')
+    cpf = st.text_input('Insira o CPF para ser validado', placeholder = '999.999.999-99', max_chars = 14)
+    container = st.container()
+    if st.button('Validar'):
+        with container:
+            if cpf_tool.cpf_validator(cpf):
+                st.success('CPF Válido!', icon="✅")
+            else:
+                st.error('CPF Inválido', icon="🚨")
+
+def Gerar_CPF():
+    st.subheader('Gerador de CPF')
+    estado = st.selectbox('Escolha o estado', ['QUALQUER UM'] + ufbr.list_uf)
+    formatar =  st.radio('Formatar CPF', ['Sim', 'Não'], horizontal = True)
+    container = st.container()
+    if st.button('Gerar CPF'):
+        cpf = cpf_tool.cpf_generator(estado = estado)
+        with container:
+            if formatar == 'Sim':
+                st.markdown('### CPF Formatado')
+                st.markdown(f'#### {cpf_tool.cpf_formatter(cpf)}')
+            else:
+                st.markdown('### CPF Não Formatado')
+                st.markdown(f'### {cpf}')
+
+if __name__ == '__main__':
+    App()
--- cpf.py
+++ cpf.py
@@ -1,7 +1,37 @@
 from random import randint as rand
 
 class CPF:
-    def cpf_validator(cpf):
+    def __init__(self):
+        self.tabela = {
+            'AC': 2,
+            'AL': 4,
+            'AP': 2,
+            'AM': 2,
+            'BA': 5,
+            'CE': 3,
+            'DF': 0,
+            'ES': 7,
+            'GO': 1,
+            'MA': 3,
+            'MT': 1,
+            'MS': 1,
+            'MG': 6,
+            'PA': 2,
+            'PB': 4,
+            'PR': 9,
+            'PE': 4,
+            'PI': 3,
+            'RJ': 7,
+            'RN': 4,
+            'RS': 0,
+            'RO': 2,
+            'RR': 2,
+            'SC': 9,
+            'SP': 8,
+            'SE': 5,
+            'TO': 1
+        }
+    def cpf_validator(self, cpf):
         cpf = [int(x) for x in cpf if x.isnumeric()]
         if True in [len(cpf) != 11, cpf == cpf[::-1]]:
             return False
@@ -10,12 +40,17 @@ class CPF:
                 return False
         return True
 
-    def cpf_generator():
+    def cpf_generator(self, estado = False):
         while True:
             cpf = [rand(0, 9) for x in range(9)]
+            if estado in self.tabela:
+                cpf[8] = self.tabela[estado]
             if cpf != cpf[::-1]:
                 break
         for y in [10, 11]:
             cpf.append(sum([cpf[y - x] * x for x in range(y, 1, -1)]) * 10 % 11 % 10)
         cpf = ''.join([str(x) for x in cpf])
+        return cpf
+
+    def cpf_formatter(self, cpf):
         return f'{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}'
