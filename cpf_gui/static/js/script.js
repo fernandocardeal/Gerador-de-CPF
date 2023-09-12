916db9b4047a709ecb923232c4d89d2a63f06d98
Luis Fernando Santos Cardeal <fernandocardeal529@gmail.com>
2023-09-12 01:38:25 -0300

    Commit 11: Correção da função de formatar CPF, no caso de CPF inserido manualmente pelo usuário para validação

--- cpf_gui/static/js/script.js
+++ cpf_gui/static/js/script.js
@@ -1,6 +1,5 @@
-var cpf_formatado = "000.000.000-00"
-var cpf_nao_formatado = "00000000000"
-document.getElementById("cpf-box").value = cpf_formatado
+var cpfData = ["00000000000","000.000.000-00"]
+document.getElementById("cpf-box").value = cpfData[0]
 
 const gerarCpf = () => {
     fetch("http://127.0.0.1:8000/api/cpf/gerar/")
@@ -12,9 +11,8 @@ const gerarCpf = () => {
     })
     .then(data => {
         document.getElementById("cpf-box").style.borderColor = "white"
-        mostrarCpf(coletarCpf() === cpf_nao_formatado ? data[0]: data[1])
-        cpf_nao_formatado = data[0]
-        cpf_formatado = data[1]
+        mostrarCpf(coletarCpf() === cpfData[0] ? data[0]: data[1])
+        cpfData = Object.values(data)
     })
     .catch(error => {
         console.error("Erro:", error)
@@ -26,7 +24,6 @@ const mostrarCpf = (cpf) => {
     cpfBox.value = cpf
 }
 
-
 const coletarCpf = () => {
     let cpfBox = document.getElementById("cpf-box")
     return cpfBox.value
@@ -34,7 +31,15 @@ const coletarCpf = () => {
 
 const formatarCpf = () => {
     let cpf = coletarCpf()
-    mostrarCpf(cpf === cpf_nao_formatado ? cpf_formatado: cpf_nao_formatado)
+    if(cpfData.indexOf(cpf) !== -1) {
+        mostrarCpf(cpf === cpfData[0] ? cpfData[1]: cpfData[0])
+    } else {
+        cpfData = [
+            cpf.replace(/\D/g, ""), 
+            cpf.replace(/\D/g, "").replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4')
+        ]
+        mostrarCpf(cpf === cpfData[0] ? cpfData[1]: cpfData[0])
+    }
 }
 
 const validarCpf = () => {
@@ -48,11 +53,7 @@ const validarCpf = () => {
     })
     .then(data => {
         let cpfBox = document.getElementById("cpf-box")
-        if(data["estado"]) {
-            cpfBox.style.borderColor = "limegreen"
-        } else {
-            cpfBox.style.borderColor = "red"
-        }
+        cpfBox.style.borderColor = data["estado"] ? "limegreen": "red"
     })
     .catch(error => {
         console.error("Erro:", error)
